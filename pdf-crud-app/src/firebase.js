55c243aed759b6ca1e99590a8ce3243a8e84a296
Kshitijpawar <kshitijvijay271199@gmail.com>
2024-12-28 13:56:10 +0530

    refactor: update Firebase configuration to use project-specific database URL

--- pdf-crud-app/src/firebase.js
+++ pdf-crud-app/src/firebase.js
@@ -10,7 +10,8 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_RTDB,
+  // databaseURL: process.env.REACT_APP_FIREBASE_RTDB,
+  databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,

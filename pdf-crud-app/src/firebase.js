2b29d8d4aa8f1dbe5f318fe2492558a4a2c6214e
Kshitijpawar <kshitijvijay271199@gmail.com>
2024-12-28 21:59:37 +0530

    refactor: export firebase configuration for improved modularity

--- pdf-crud-app/src/firebase.js
+++ pdf-crud-app/src/firebase.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   // databaseURL: process.env.REACT_APP_FIREBASE_RTDB,

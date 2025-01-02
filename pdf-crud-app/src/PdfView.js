c89a0a264a6a8bec2806904b22e053b025b991a1
Kshitijpawar <kshitijvijay271199@gmail.com>
2025-01-02 14:58:52 +0530

    feat: refine auto-scrolling functionality and adjust scroll speed control in PDF viewer

--- pdf-crud-app/src/PdfView.js
+++ pdf-crud-app/src/PdfView.js
@@ -1,5 +1,4 @@
-// on mobile devices the pdf doesn't stop scrolling even after reaching the end of page.
-// I cannot find a way to stop the scrolling.
+
 
 import React, { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
@@ -10,13 +9,14 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/b
 const PdfView = () => {
   let globalId;
 
-  const scrollSpeed = useRef(2);
+  const scrollSpeed = useRef(0.6);
 
   const location = useLocation();
   const pdfUrl = location.state || { default: "No data passed" };
 
   const [numPages, setNumPages] = useState(0);
   const scaleRef = useRef(1); // For dynamic scaling
+  const [scale, setScale] = useState(1);
   const pageRefs = useRef([]); // To hold references to individual pages
 
   //   const isScrolling = useRef(false); // Tracks if scrolling was triggered programmatically
@@ -32,40 +32,40 @@ const PdfView = () => {
     const updateScale = () => {
       const screenWidth = window.innerWidth;
       const calculateScale =
-        screenWidth < 600 ? 0.5 : screenWidth < 900 ? 0.8 : 1;
-      scaleRef.current = calculateScale;
-      console.log(scaleRef.current);
+        screenWidth < 600 ? 0.5 : screenWidth < 900 ? 0.8 : 1.75;
+      //   scaleRef.current = calculateScale;
+      setScale(calculateScale);
+      console.log(scale);
     };
 
     updateScale();
     window.addEventListener("resize", updateScale);
 
     return () => window.removeEventListener("resize", updateScale);
-  }, []);
+  }, [scale]);
 
   const autoScroll = () => {
-    console.log("auto scroll function");
+    // console.log("auto scroll function");
     const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
     if (Math.ceil(scrollTop) + clientHeight >= scrollHeight) {
       console.log("reached end of page cancelling scroll");
       cancelAnimationFrame(globalId);
       return;
     }
-
+    // console.log("current scroll speed", scrollSpeed.current);
     window.scrollBy(0, scrollSpeed.current);
     globalId = requestAnimationFrame(autoScroll);
   };
   const handleStartScroll = () => {
-    console.log("inside start scroll");
+    // console.log("inside start scroll");
 
     globalId = requestAnimationFrame(autoScroll);
   };
   const handleStopScroll = () => {
-    console.log("inside stop scroll");
+    // console.log("inside stop scroll");
     cancelAnimationFrame(globalId);
   };
 
-  
   const handleScrollSpeedChange = (e) => {
     scrollSpeed.current = e.target.value;
   };
@@ -85,7 +85,8 @@ const PdfView = () => {
                 renderAnnotationLayer={false}
                 renderTextLayer={false}
                 className="pdf-page"
-                scale={scaleRef.current}
+                // scale={scaleRef.current}
+                scale={scale}
               />
             </div>
           ))}
@@ -97,9 +98,9 @@ const PdfView = () => {
           id="scroll-speed"
           type="range"
           min="0"
-          max="5"
-          step="0.1"
-          defaultValue={0.7}
+          max="1"
+          step="0.0005"
+          defaultValue={scrollSpeed.current}
           onChange={handleScrollSpeedChange}
         />
         <button onClick={handleStartScroll}>Start</button>
